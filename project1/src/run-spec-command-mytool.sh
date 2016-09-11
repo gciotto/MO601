a5617867c3fb6e640f54ccdb85868774578183d4
Gustavo Ciotto <gustavociotto@gmail.com>
2016-09-11 14:45:47 -0300

    unnecessary line removed

--- project1/src/run-spec-command-mytool.sh
+++ project1/src/run-spec-command-mytool.sh
@@ -3,7 +3,6 @@
 PIN_ROOT=/home/gciotto/pin-3.0-76991-gcc-linux
 PROJECT_DIR=/home/gciotto/workspace/MO601-117136/Projeto-1/item-d
 TOOL_NAME=bp-pintool
-#TOOL_ROOT=${PROJECT_DIR}/obj-intel64/most-used-ins.so
 TOOL_ROOT=${PROJECT_DIR}/obj-intel64/${TOOL_NAME}.so
 
 ${PIN_ROOT}/pin -t ${TOOL_ROOT} -o ${PROJECT_DIR}/${TOOL_NAME}.tmp.log -- $1
