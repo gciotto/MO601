a3aa24b546b692f5b71652bad864bc6224560dd4
gciotto <gustavociotto@gmail.com>
2016-10-10 22:06:54 -0300

    different sized L1 L2

--- project2/src/page_simulator_tool.cpp
+++ project2/src/page_simulator_tool.cpp
@@ -10,13 +10,7 @@ typedef UINT32 CACHE_STATS; // type of cache hit/miss counters
 
 #define TABLE_TREE_LEVEL 3 
 
-enum page_size {
-	PAGE_4KB,
-	PAGE_4MB,
-
-	N_PAGE
-};
-
+/* TLB 4KB PAGES */
 namespace TLB_4K
 {
 // TLB: 4 kB pages, 512 entries, fully associative
@@ -33,6 +27,7 @@ typedef CACHE_ROUND_ROBIN(max_sets, max_associativity, allocation) CACHE;
 LOCALVAR TLB_4K::CACHE itlb_4k("ITLB_4K", TLB_4K::cacheSize, TLB_4K::lineSize, TLB_4K::associativity);
 LOCALVAR TLB_4K::CACHE dtlb_4k("DTLB_4K", TLB_4K::cacheSize, TLB_4K::lineSize, TLB_4K::associativity);
 
+/* TLB 4MB PAGES */
 namespace TLB_4M
 {
 // TLB: 4 MB pages, 512 entries, fully associative
@@ -48,6 +43,66 @@ typedef CACHE_ROUND_ROBIN(max_sets, max_associativity, allocation) CACHE;
 }
 LOCALVAR TLB_4M::CACHE itlb_4m("ITLB_4M", TLB_4M::cacheSize, TLB_4M::lineSize, TLB_4M::associativity);
 LOCALVAR TLB_4M::CACHE dtlb_4m("DTLB_4M", TLB_4M::cacheSize, TLB_4M::lineSize, TLB_4M::associativity);
+
+namespace L1_4K
+{
+    // 1st level instruction cache: 8 MB, 4 KB lines, 8-way associative
+    const UINT32 cacheSize = 64*KILO;
+    const UINT32 lineSize = 4*KILO;
+    const UINT32 associativity = 4;
+    const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_NO_ALLOCATE;
+
+    const UINT32 max_sets = cacheSize / (lineSize * associativity);
+    const UINT32 max_associativity = associativity;
+
+    typedef CACHE_ROUND_ROBIN(max_sets, max_associativity, allocation) CACHE;
+}
+LOCALVAR L1_4K::CACHE il1_4k("L1 4KB Instruction Cache", L1_4K::cacheSize, L1_4K::lineSize, L1_4K::associativity);
+LOCALVAR L1_4K::CACHE dl1_4k("D1 4KB Instruction Cache", L1_4K::cacheSize, L1_4K::lineSize, L1_4K::associativity);
+
+namespace L1_4M
+{
+    // 1st level instruction cache: 32 kB, 4 KB lines, 32-way associative
+    const UINT32 cacheSize = 64*MEGA;
+    const UINT32 lineSize = 4*MEGA;
+    const UINT32 associativity = 4;
+    const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_ALLOCATE;
+
+    const UINT32 max_sets = cacheSize / (lineSize * associativity);
+    const UINT32 max_associativity = associativity;
+
+    typedef CACHE_ROUND_ROBIN(max_sets, max_associativity, allocation) CACHE;
+}
+LOCALVAR L1_4M::CACHE il1_4m("L1 4MB Instruction Cache", L1_4M::cacheSize, L1_4M::lineSize, L1_4M::associativity);
+LOCALVAR L1_4M::CACHE dl1_4m("D1 4MB Instruction Cache", L1_4M::cacheSize, L1_4M::lineSize, L1_4M::associativity);
+
+namespace UL2_4K
+{
+    // 2nd level unified cache: 2 MB, 64 B lines, direct mapped
+    const UINT32 cacheSize = 256*KILO;
+    const UINT32 lineSize = 4*KILO;
+    const UINT32 associativity = 1;
+    const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_ALLOCATE;
+
+    const UINT32 max_sets = cacheSize / (lineSize * associativity);
+
+    typedef CACHE_DIRECT_MAPPED(max_sets, allocation) CACHE;
+}
+LOCALVAR UL2_4K::CACHE ul2_k("L2 4KB Unified Cache", UL2_4K::cacheSize, UL2_4K::lineSize, UL2_4K::associativity);
+
+namespace UL2_4M
+{
+    // 2nd level unified cache: 2 MB, 64 B lines, direct mapped
+    const UINT32 cacheSize = 256*MEGA;
+    const UINT32 lineSize = 4*MEGA;
+    const UINT32 associativity = 1;
+    const CACHE_ALLOC::STORE_ALLOCATION allocation = CACHE_ALLOC::STORE_ALLOCATE;
+
+    const UINT32 max_sets = cacheSize / (lineSize * associativity);
+
+    typedef CACHE_DIRECT_MAPPED(max_sets, allocation) CACHE;
+}
+LOCALVAR UL2_4M::CACHE ul2_m("L2 4MB Unified Cache", UL2_4M::cacheSize, UL2_4M::lineSize, UL2_4M::associativity);
 
 /* Output file handler */
 ofstream OutFile;
@@ -75,38 +130,61 @@ LOCALFUN VOID Fini(int code, VOID * v)
 	OutFile << itlb_4k;
 	OutFile << dtlb_4k;
 
-	OutFile << "Instruction memory accesses:" << endl;
-	OutFile << "|--- (4MB) " << itlb_4m.Misses() * TABLE_TREE_LEVEL << endl;
-	OutFile << "|--- (4KB) " << itlb_4k.Misses() * TABLE_TREE_LEVEL << endl;
+	OutFile << il1_4k;
+	OutFile << dl1_4k;
+	OutFile << il1_4m;
+	OutFile << dl1_4m;
 
+	OutFile << ul2_k;
+	OutFile << ul2_m;
 
-	OutFile << "Data memory accesses:" << endl;
-	OutFile << "|--- (4MB) " << dtlb_4m.Misses() * TABLE_TREE_LEVEL << endl;
-	OutFile << "|--- (4KB) " << dtlb_4k.Misses() * TABLE_TREE_LEVEL << endl;
 }
 
 LOCALFUN VOID checkDataTLB(ADDRINT addr, UINT32 size, BOOL access_type) {
 
 	CACHE_BASE::ACCESS_TYPE _access = access_type ? CACHE_BASE::ACCESS_TYPE_LOAD : CACHE_BASE::ACCESS_TYPE_STORE;
 
+	BOOL hit_4k, hit_4m;
+
 	if (size <= 4) {
 
 		dtlb_4k.AccessSingleLine(addr, _access);
 		dtlb_4m.AccessSingleLine(addr, _access);
+		
+		hit_4k = dl1_4k.AccessSingleLine(addr, _access);
+		hit_4m = dl1_4m.AccessSingleLine(addr, _access);
+
+		if (!hit_4k) ul2_k.Access(addr, size, _access);
+		if (!hit_4m) ul2_m.Access(addr, size, _access);
+			
 	}
 	else {
 
 		dtlb_4k.Access(addr, size, _access);
 		dtlb_4m.Access(addr, size, _access);
+
+		hit_4k = dl1_4k.Access(addr, size, _access);
+		hit_4m = dl1_4m.Access(addr, size, _access);
+
+		if (!hit_4k) ul2_k.Access(addr, size, _access);
+		if (!hit_4m) ul2_m.Access(addr, size, _access);
 	}
 
 }
 
 LOCALFUN VOID BBL_analysisCallBack (ADDRINT addr, UINT32 size) {
 
+	BOOL hit_4k, hit_4m;
+
 	itlb_4k.Access(addr, size, CACHE_BASE::ACCESS_TYPE_LOAD);
 
 	itlb_4m.Access(addr, size, CACHE_BASE::ACCESS_TYPE_LOAD);
+
+	hit_4k = il1_4k.Access(addr, size, CACHE_BASE::ACCESS_TYPE_LOAD);
+	hit_4m = il1_4m.Access(addr, size, CACHE_BASE::ACCESS_TYPE_LOAD);
+
+	if (!hit_4k) ul2_k.Access(addr, size, CACHE_BASE::ACCESS_TYPE_LOAD);
+	if (!hit_4m) ul2_m.Access(addr, size, CACHE_BASE::ACCESS_TYPE_LOAD);
 }
 
 LOCALFUN VOID initTraceCallBack(TRACE trace, VOID *v) {
