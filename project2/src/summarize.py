0252e542aed9826baf1aa827e677b3a6ed13824d
gciotto <gustavociotto@gmail.com>
2016-10-11 19:26:53 -0300

    add ul3 miss count

--- project2/src/summarize.py
+++ project2/src/summarize.py
@@ -3,8 +3,8 @@
 import os
 
 project_dir = "/home/gciotto/MO601-117136/project2/src/"
-benchmarks = ["410.bwaves", "429.mcf", "434.zeusmp", "436.cactusADM", "459.GemsFDTD", "400.perlbench", "401.bzip2", "481.wrf", "433.milc", "403.gcc"]
-suffix = "06_10"
+benchmarks = ["410.bwaves", "429.mcf", "434.zeusmp", "436.cactusADM", "459.GemsFDTD", "400.perlbench", "401.bzip2", "481.wrf", "433.milc", "403.gcc", "toy"]
+suffix = "11_10_2"
 
 dict = {}
 
@@ -21,44 +21,56 @@ if __name__ == '__main__' :
 		
 		for (dirpath, dirnames, filenames) in os.walk(project_dir + benchmark):
 			for filename in filenames:
-				begin = filename.index("_0-") + 3
-				end = filename.index(".log") 
 
-				region_ratio = float(filename [begin : end])
-
-				f = open(project_dir + benchmark + "/" + filename,'r')
-
-				for line in f.readlines() :
+				if suffix in filename:
 					
-					if not ":" in line : continue
-					
-					line =  line.strip().split()
+					print filename
 
-					if "TLB" in line[0]:
-						key = line[0][: line[0].index(":")]
+					if benchmark == "toy" :
+						region_ratio = 1.0
+					else:
+						begin = filename.index("_0-") + 3
+						end = filename.index(".log") 
+
+						region_ratio = float(filename [begin : end]) / 1e5
+
+					f = open(project_dir + benchmark + "/" + filename,'r')
+
+					for line in f.readlines() :
+					
+						if not ":" in line : continue
+					
+						line =  line.strip().split()
+
+						if "TLB" in line[0] or line[0] in [ "Instruction_Memory:", "Data_memory:" ]:
+
+							key = line[0][: line[0].index(":")]
 						
-						if not key in dict[benchmark]:
-							dict[benchmark][key] = {}
+							if not key in dict[benchmark]:
+								dict[benchmark][key] = {}
 
-						continue
+							continue
 					
-					if line[0] in ['Store', 'Load', 'Total']:
+						if line[0] in ['Store', 'Load', 'Total']:
 
-						_access_type = line[1][:-1]
+							_access_type = line[1][:-1]
 
-						if _access_type in ['Hits', 'Misses', 'Accesses'] :
+							if _access_type in ['Hits', 'Misses', 'Accesses'] :
 						
-							if not (line[0] + _access_type) in dict[benchmark][key]:
-								dict[benchmark][key][line[0] + _access_type] = 0
+								if not (line[0] + _access_type) in dict[benchmark][key]:
+									dict[benchmark][key][line[0] + _access_type] = 0
 
-							dict[benchmark][key][line[0] + _access_type] = dict[benchmark][key][line[0] + _access_type] + region_ratio * float (line[2])
+								dict[benchmark][key][line[0] + _access_type] = dict[benchmark][key][line[0] + _access_type] + region_ratio * float (line[2])
 					
 		print benchmark
-		print dict[benchmark]		
+		# print dict[benchmark]		
 
-		
-		f_KB.write(benchmark[4:] + '.ref1,')
-		f_MB.write(benchmark[4:] + '.ref1,')
+		if benchmark == "toy":
+			f_KB.write(benchmark + ',')
+			f_MB.write(benchmark + ',')
+		else:
+			f_KB.write(benchmark[4:] + '.ref1,')
+			f_MB.write(benchmark[4:] + '.ref1,')
 	
 		itlb_4KB = dict[benchmark]["ITLB_4K"]
 		itlb_4MB = dict[benchmark]["ITLB_4M"]
@@ -66,11 +78,14 @@ if __name__ == '__main__' :
 		dtlb_4KB = dict[benchmark]["DTLB_4K"]
 		dtlb_4MB = dict[benchmark]["DTLB_4M"]
 
-		f_KB.write("%d,%d,%d," % ((itlb_4KB["TotalAccesses"] + 3*itlb_4KB["TotalMisses"]), itlb_4KB["TotalMisses"], 3*itlb_4KB["TotalMisses"]))
-		f_KB.write("%d,%d,%d,\n" % ((dtlb_4KB["TotalAccesses"] + 3*dtlb_4KB["TotalMisses"]), dtlb_4KB["TotalMisses"], 3*dtlb_4KB["TotalMisses"]))
+		im = dict[benchmark]["Instruction_Memory"]
+		dm = dict[benchmark]["Data_memory"]
 
-		f_MB.write("%d,%d,%d," % ((itlb_4MB["TotalAccesses"] + 3*itlb_4MB["TotalMisses"]), itlb_4MB["TotalMisses"], 3*itlb_4MB["TotalMisses"]))
-		f_MB.write("%d,%d,%d,\n" % ((dtlb_4MB["TotalAccesses"] + 3*dtlb_4MB["TotalMisses"]), dtlb_4MB["TotalMisses"], 3*dtlb_4MB["TotalMisses"]))
+		f_KB.write("%d,%d,%d," % ((im["TotalMisses"] + 3*itlb_4KB["TotalMisses"]), itlb_4KB["TotalMisses"], 3*itlb_4KB["TotalMisses"]))
+		f_KB.write("%d,%d,%d,\n" % ((dm["TotalMisses"] + 3*dtlb_4KB["TotalMisses"]), dtlb_4KB["TotalMisses"], 3*dtlb_4KB["TotalMisses"]))
+
+		f_MB.write("%d,%d,%d," % ((im["TotalMisses"] + 3*itlb_4MB["TotalMisses"]), itlb_4MB["TotalMisses"], 3*itlb_4MB["TotalMisses"]))
+		f_MB.write("%d,%d,%d,\n" % ((dm["TotalMisses"] + 3*dtlb_4MB["TotalMisses"]), dtlb_4MB["TotalMisses"], 3*dtlb_4MB["TotalMisses"]))
 		
 	f_KB.close()
 	f_MB.close()
