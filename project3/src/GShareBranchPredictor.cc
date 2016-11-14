e454eefd598a61ac047c5fe1522e94c7870d1daf
Gustavo Ciotto <gustavociotto@gmail.com>
2016-11-14 15:32:05 -0200

    successfully colpiled

--- project3/src/GShareBranchPredictor.cc
+++ project3/src/GShareBranchPredictor.cc
@@ -1,5 +1,4 @@
 #include "GShareBranchPredictor.h"
-#include <math.h>
 
 GShareBranchPredictor::GShareBranchPredictor(String name, core_id_t core_id, UInt32 entries, UInt32 b_history)
     : BranchPredictor(name, core_id)
@@ -8,16 +7,26 @@ GShareBranchPredictor::GShareBranchPredictor(String name, core_id_t core_id, UIn
     , g_history(0)
 {
     h_mask = 1;
-    for (unsigned int i = 0; i < entries; i++) 
-        h_mask *= 2;
 
-    h_mask -= 1;
+    LOG_PRINT_WARNING("entries: %d",  m_num_entries);
+
+    for (unsigned int i = 0; i < b_history; i++) 
+        h_mask = h_mask * 2;
+
+    h_mask = h_mask - 1;
+
+    LOG_PRINT_WARNING("Gshare: %d", h_mask);
+}
+
+GShareBranchPredictor::~GShareBranchPredictor() {
+    m_predictors.clear();
 }
 
 bool GShareBranchPredictor::predict(IntPtr ip, IntPtr target) {
 
     return true;
 }
+
 
 BranchPredictorReturnValue GShareBranchPredictor::lookup(IntPtr ip, IntPtr target) {
 
@@ -31,6 +40,9 @@ BranchPredictorReturnValue GShareBranchPredictor::lookup(IntPtr ip, IntPtr targe
 }
 
 void GShareBranchPredictor::update(bool predicted, bool actual, IntPtr ip, IntPtr target) {
+
+    updateCounters(predicted, actual);
+
     UInt32 index = (ip ^ g_history) & h_mask;
 
     g_history = (g_history << 1) | actual;
--- project3/src/GShareBranchPredictor.h
+++ project3/src/GShareBranchPredictor.h
@@ -1,7 +1,6 @@
 #ifndef GSHARE_PREDICTOR_H
 #define GSHARE_PREDICTOR_H
 
-
 #include <vector>
 #include <stdint.h>
 
@@ -10,11 +9,15 @@
 #include "branch_predictor_return_value.h"
 #include "saturating_predictor.h"
 
-class GShareBranchPredictor : BranchPredictor {
+class GShareBranchPredictor : public BranchPredictor {
 
 public:
-    GShareBranchPredictor(String name, core_id_t core_id, UInt32 entries, UInt32 history);
+    GShareBranchPredictor(String name, core_id_t core_id, UInt32 entries, UInt32 b_history);
     ~GShareBranchPredictor();
+
+    bool predict(IntPtr ip, IntPtr target);
+    BranchPredictorReturnValue lookup(IntPtr ip, IntPtr target);
+    void update(bool predicted, bool actual, IntPtr ip, IntPtr target);
 
 private:
    
@@ -22,6 +25,6 @@ private:
     UInt32 m_num_entries;
     UInt32 g_history;
     UInt32 h_mask;
-}
+};
 
 #endif /* GSHARE_PREDICTOR_H */
